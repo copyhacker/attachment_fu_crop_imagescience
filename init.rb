f5ef1379e7a426679c681658edaea34bb70f79a4
Jonathan Vaught <jonathan.vaught@gmail.com>
2009-02-17 14:34:30 -0500

    Updated for latest attachment_fu version (temp_path= was removed)

--- init.rb
+++ init.rb
@@ -45,21 +45,30 @@ Geometry.module_eval do
 
     [new_width, new_height].collect! { |v| v.round }
   end
-  
 end
 
-Technoweenie::AttachmentFu::Processors::ImageScienceProcessor.module_eval do
 
+Technoweenie::AttachmentFu::Processors::ImageScienceProcessor.module_eval do
   def resize_image(img, size)
     # create a dummy temp file to write to
-    self.temp_path = write_to_temp_file(filename)
+    
+    # ImageScience doesn't handle all gifs properly, so it converts them to
+    # pngs for thumbnails.  It has something to do with trying to save gifs
+    # with a larger palette than 256 colors, which is all the gif format
+    # supports.
+    filename.sub! /gif$/, 'png'
+    content_type.sub!(/gif$/, 'png')
+    
+    temp_paths.unshift write_to_temp_file(filename)
+    
     grab_dimensions = lambda do |img|
       self.width  = img.width  if respond_to?(:width)
       self.height = img.height if respond_to?(:height)
-      img.save temp_path
+      img.save self.temp_path
       self.size = File.size(self.temp_path)
       callback_with_args :after_resize, img
     end
+    
     size = size.first if size.is_a?(Array) && size.length == 1
     if size.is_a?(Fixnum) || (size.is_a?(Array) && size.first.is_a?(Fixnum))
       if size.is_a?(Fixnum)
@@ -68,20 +77,20 @@ Technoweenie::AttachmentFu::Processors::ImageScienceProcessor.module_eval do
         img.resize(size[0], size[1], &grab_dimensions)
       end
     else
-      n_size = [img.width, img.height] / size.to_s
+      new_size = [img.width, img.height] / size.to_s
+      
       if size.ends_with? "!"
-        aspect = n_size[0].to_f / n_size[1].to_f
+        aspect = new_size[0].to_f / new_size[1].to_f
         ih, iw = img.height, img.width
         w, h = (ih * aspect), (iw / aspect)
-        w = [iw, w].min.to_i
-        h = [ih, h].min.to_i
-        img.with_crop( (iw-w)/2, (ih-h)/2, (iw+w)/2, (ih+h)/2) {
-          |crop| crop.resize(n_size[0], n_size[1], &grab_dimensions )
+        w = [ iw, w ].min.to_i
+        h = [ ih, h ].min.to_i
+        img.with_crop((iw - w) / 2, (ih - h) / 2, (iw + w) / 2, (ih + h) / 2) {
+          |crop| crop.resize(new_size[0], new_size[1], &grab_dimensions )
         }
       else
-        img.resize(n_size[0], n_size[1], &grab_dimensions)
+        img.resize(new_size[0], new_size[1], &grab_dimensions)
       end
     end
   end
-
 end
